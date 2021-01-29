c5fa3349a3ad068bf50ffd303bbb87314b2864c7
nanhanglim <hello@nanhanglim.com>
2021-01-30 03:00:11 +0800

    Delete home.js

--- LICENSE.md/home.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  var heroVid = document.querySelector("#sh-hero-vid");
-  heroVid.innerHTML = "<source src=\"/wp-content/uploads/2021/01/hero.mp4\">";
-  var heroVidPlay = heroVid.play();
-});
