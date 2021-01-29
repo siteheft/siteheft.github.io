0c97cc73764654a1e1520ea912b408e0806aa365
nanhanglim <hello@nanhanglim.com>
2021-01-30 03:01:04 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,3 +1,5 @@
+/* COPYRIGHT 2021 SITEHEFT */
+
 $(document).ready(function() {
   var heroVid = document.querySelector("#sh-hero-vid");
   heroVid.innerHTML = "<source src=\"/wp-content/uploads/2021/01/hero.mp4\">";
