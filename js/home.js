a0b0c3ec0e4ef9c82a86b669daa64577c5a22f0f
nanhanglim <hello@nanhanglim.com>
2021-01-31 01:58:29 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -4,4 +4,22 @@ $(document).ready(function() {
   var heroVid = document.querySelector("#sh-hero-vid");
   heroVid.innerHTML = "<source src=\"/wp-content/uploads/2021/01/hero.mp4\">";
   var heroVidPlay = heroVid.play();
+  heroVid.onplaying = () => {
+    startHeroTextChange()
+  }
 });
+
+function startHeroTextChange() {
+  var heroTextArray = ["your photos", "your dish", "your creativity", "your moves", "your craft", "your adventures"];
+  var counter = 0;
+  var text = document.querySelector("#shHeroChangingText").innerText
+  setTimeout(() => {
+    if (counter == 5) {
+      counter = 0;
+      text = heroTextArray[counter];
+    } else {
+      counter += 1
+      text = heroTextArray[counter];
+    }
+  }, 7000)
+}
