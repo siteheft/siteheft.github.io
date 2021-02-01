82ea111b4249147d8a05d96d755910af0333eccf
nanhanglim <hello@nanhanglim.com>
2021-02-02 00:56:10 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
   var heroVid = document.querySelector("#sh-hero-vid");
-  heroVid.innerHTML = "<source src=\"/wp-content/uploads/2021/01/hero.mp4\">";
+  heroVid.innerHTML = "<source src=\"/wp-content/uploads/2021/02/hero.mp4\">";
   var heroVidPlay = heroVid.play();
   heroVid.onplaying = startHeroTextChange()
 });
