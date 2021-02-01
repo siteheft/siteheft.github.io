b71a6ac696bc584afb1befacf5d87bbbcb5bfb4c
nanhanglim <hello@nanhanglim.com>
2021-02-02 02:03:58 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -18,14 +18,14 @@ function startHeroTextChange() {
       document.querySelector("#shHeroChangingText").classList.add("fade-out");
       setTimeout(() => {
         document.querySelector("#shHeroChangingText").classList.remove("fade-out");
-      }, 1400)
+      }, 1350)
     } else {
       counter += 1
       document.querySelector("#shHeroChangingText").innerText = heroTextArray[counter];
       document.querySelector("#shHeroChangingText").classList.add("fade-out");
       setTimeout(() => {
         document.querySelector("#shHeroChangingText").classList.remove("fade-out");
-      }, 1400)
+      }, 1350)
     }
   }, 7000)
 }
