85584dc380f2a0456d1bc5338aa6e59c0630ff43
nanhanglim <hello@nanhanglim.com>
2021-02-02 00:58:51 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -18,7 +18,7 @@ function startHeroTextChange() {
       document.querySelector("#shHeroChangingText").classList.add("fadeChangingText");
       setTimeout(() => {
         document.querySelector("#shHeroChangingText").classList.remove("fadeChangingText");
-      }, 1000)
+      }, 500)
     } else {
       counter += 1
       document.querySelector("#shHeroChangingText").innerText = heroTextArray[counter];
