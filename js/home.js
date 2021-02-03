2495d3c36dabd609b21c109658c5f5bf9d551d0e
nanhanglim <hello@nanhanglim.com>
2021-02-03 09:20:31 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -13,18 +13,18 @@ var checkHeroVidLoadedInterval = setInterval(() => {
 function startHeroTextChange() {
   var heroTextArray = ["your photos", "your dish", "your creativity", "your moves", "your craft", "your adventures"];
   heroVid.addEventListener("timeupdate", () => {
-    if (heroVid.currentTime >= 0 && heroVid.currentTime < 1) {
-      document.querySelector("#shHeroChangingText").innerText = heroTextArray[0];
-    } else if (heroVid.currentTime >= 7 && heroVid.currentTime < 8) {
+    if (heroVid.currentTime >= 6.5 && heroVid.currentTime < 8) {
       document.querySelector("#shHeroChangingText").innerText = heroTextArray[1];
-    } else if (heroVid.currentTime >= 14 && heroVid.currentTime < 15) {
+    } else if (heroVid.currentTime >= 15.5 && heroVid.currentTime < 15) {
       document.querySelector("#shHeroChangingText").innerText = heroTextArray[2];
-    } else if (heroVid.currentTime >= 21 && heroVid.currentTime < 22) {
+    } else if (heroVid.currentTime >= 20.5 && heroVid.currentTime < 22) {
       document.querySelector("#shHeroChangingText").innerText = heroTextArray[3];
-    } else if (heroVid.currentTime >= 28 && heroVid.currentTime < 29) {
+    } else if (heroVid.currentTime >= 27.5 && heroVid.currentTime < 29) {
       document.querySelector("#shHeroChangingText").innerText = heroTextArray[4];
-    } else if (heroVid.currentTime >= 35 && heroVid.currentTime < 36) {
+    } else if (heroVid.currentTime >= 34.5 && heroVid.currentTime < 36) {
       document.querySelector("#shHeroChangingText").innerText = heroTextArray[5];
+    } else if (heroVid.currentTime >= 41.5 && heroVid.currentTime < 43) {
+      document.querySelector("#shHeroChangingText").innerText = heroTextArray[0];
     } 
   }, false)
 }
