d9f70e3a227c93466ef669c218c2319877afcbb1
nanhanglim <hello@nanhanglim.com>
2021-02-03 09:22:32 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -15,7 +15,7 @@ function startHeroTextChange() {
   heroVid.addEventListener("timeupdate", () => {
     if (heroVid.currentTime >= 6.5 && heroVid.currentTime < 8) {
       document.querySelector("#shHeroChangingText").innerText = heroTextArray[1];
-    } else if (heroVid.currentTime >= 15.5 && heroVid.currentTime < 15) {
+    } else if (heroVid.currentTime >= 13.5 && heroVid.currentTime < 15) {
       document.querySelector("#shHeroChangingText").innerText = heroTextArray[2];
     } else if (heroVid.currentTime >= 20.5 && heroVid.currentTime < 22) {
       document.querySelector("#shHeroChangingText").innerText = heroTextArray[3];
