ae37cba75d84314f30f51af7b22b616caad8fe43
nanhanglim <hello@nanhanglim.com>
2021-03-02 01:57:20 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -409,7 +409,7 @@ function navEmailClick() {
         <div class="sh-nav-dropdown-right">
             <span class="sh-nav-dropdown-title">Why you need a professional email address?</span>
             <span class="sh-nav-dropdown-subtitle">It's simple, and easy to remember</span>
-            <span class="sh-nav-dropdown-description">Email addresses ending with your domain is often shorter and easy to remember as customers can easily associate you with your brand.</span>
+            <span class="sh-nav-dropdown-description">Email addresses ending with your domain are often shorter and easy to remember as customers can easily associate you with your brand.</span>
             <span class="sh-nav-dropdown-subtitle" style="margin-top:20px">Promotes both your brand and your credibility</span>
             <span class="sh-nav-dropdown-description">Clients are more likely to trust an email sent from an email address like jane@janesflower.com rather than janesflower@gmail.com.</span>
             <a href="" class="sh-nav-dropdown-morelink"><span>Learn more about SEO</span>&nbsp;&nbsp;<i class="fas fa-arrow-right"></i></a>
